1765b56abe66ef06fe9f6a3011bae79005c055aa
Mathieu Daix <mathieudaixpro@gmail.com>
2023-10-23 10:20:41 +0200

    remove lenis

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,4 @@
 import { Inter } from 'next/font/google'
-import { ReactLenis } from '@studio-freight/react-lenis'
 import Image from 'next/image'
 import { Analytics } from '@vercel/analytics/react'
 
@@ -7,21 +6,19 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Layout({ children }) {
     return (
-        <ReactLenis root>
-            <main className={inter.className}>
-                <div className='info-top'>
-                    <Image src='/images/logo-kairos.svg' width={80} height={14} alt='Logo Kairos Agency.' />
-                    <p>next.js 3d page transition</p>
-                </div>
+        <main className={inter.className}>
+            <div className='info-top'>
+                <Image src='/images/logo-kairos.svg' width={80} height={14} alt='Logo Kairos Agency.' />
+                <p>next.js 3d page transition</p>
+            </div>
 
-                <div className='info-bottom'>
-                    <p>October 23, 2023</p>
-                </div>
+            <div className='info-bottom'>
+                <p>October 23, 2023</p>
+            </div>
 
-                {children}
+            {children}
 
-                <Analytics />
-            </main>
-        </ReactLenis>
+            <Analytics />
+        </main>
     )
 }
